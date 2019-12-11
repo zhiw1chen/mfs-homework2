73e914706eb43154519c8a55b5357807760c889b
a735315482 <735315482@qq.com>
2019-12-11 16:51:21 +0800

    1

--- mine/birdsName.js
+++ mine/birdsName.js
@@ -1,11 +1,9 @@
 let rp = require('request-promise')
 
-async function main() {
-    let res = await rp('http://h5uc.wondersea.mobi/storage/birdDetail.do?sso=MaqZ62nbP2qNHOhGEyFuTg__&vp=2&vk=iHJJy&id=1');
+async function callback(index) {
+    let res = await rp('http://h5uc.wondersea.mobi/storage/birdDetail.do?sso=MaqZ62nbP2qNHOhGEyFuTg__&vp=2&vk=iHJJy&id=' + index);
 
     let data = res.match(/(?<=<title>).*(?=详情-小鸟OL--)/);
-    console.log(data[0]);
 
+    return data[0];
 }
-
-main();

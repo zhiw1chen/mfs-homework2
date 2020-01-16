a863f96c0883fa4ee6a4a1de35036804cfc7e366
a735315482 <735315482@qq.com>
2020-01-16 18:05:11 +0800

    9

--- sign_in/routes/index.js
+++ sign_in/routes/index.js
@@ -3,7 +3,36 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+var users = [];
+users.push({
+    username: "mark123",
+    password: "qwertyuiop"
+});
+
+function usernameExist(username) {
+    for (x in users) {
+        if (users[x].username == username) {
+            console.log("用户名已存在");
+            return true;
+        }
+    }
+    console.log("用户名不存在");
+    return false;
+
+};
+
+router.post('/username-check', function(req, res, next) {
+    res.header({ "Access-Control-Allow-Origin": "*" });
+    console.log(req.body.username);
+    if (usernameExist(req.body.username)) {
+        //如果已经存在
+        res.json({ "result": 1 })
+    } else {
+        res.json({ "result": 0 })
+    }
 });
 
 module.exports = router;
